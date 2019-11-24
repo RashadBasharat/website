771b0183bb574d5743c9f68b1cc9b90fe79bc6a8
Rashad Basharat <r.basharat@newcastle.ac.uk>
2019-11-24 03:22:47 +0000

    Yes hello

--- ok/verycool/verycool/settings.py
+++ ok/verycool/verycool/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'verycool.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'yes/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
